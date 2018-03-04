cd3459b12aa68980e06991c87acd6cdb0aececd1
LukacsD88 <lukacsdavid88@gmail.com>
2018-03-04 22:01:54 +0100

    Update Folia_paratartalom_ado.ino

--- Folia_paratartalom_ado.ino
+++ Folia_paratartalom_ado.ino
@@ -5,6 +5,20 @@
 * Communication: nRF24L: used channel: 12
 * Version: 1.0
 **/
+#include "DHT.h"
+#include <SPI.h>  
+#include "RF24.h"
+
+#define DHTPIN                    4  
+#define DHTTYPE                   DHT22
+#define DHTPIN2                    5  
+#define DHTTYPE2                   DHT22  
+#define LDR_PIN                   0
+#define MAX_ADC_READING           1023
+#define ADC_REF_VOLTAGE           5.0
+#define REF_RESISTANCE            5030  // measure this for best results
+#define LUX_CALC_SCALAR           12518931
+#define LUX_CALC_EXPONENT         -1.405
 
 
 
