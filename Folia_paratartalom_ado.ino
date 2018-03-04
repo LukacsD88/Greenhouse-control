b729d5436ef1c565b318b7d382cb31d482d22fd9
LukacsD88 <lukacsdavid88@gmail.com>
2018-03-04 22:00:33 +0100

    Add files via upload

---
+++ Folia_paratartalom_ado.ino
@@ -0,0 +1,19 @@
+/*
+*
+* Folia humidity control
+* Sensors: DHT22 2pieces , LDR sensor 1 piece
+* Communication: nRF24L: used channel: 12
+* Version: 1.0
+**/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
